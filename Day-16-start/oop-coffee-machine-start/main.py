4d42de2e78c5d5f90cd3f055cac0026035f92449
Intira-72 <intira.vinit@gmail.com>
2022-06-20 10:57:19 +0700

    Day 16 OOP Coffee Machine Done

--- Day-16-start/oop-coffee-machine-start/main.py
+++ Day-16-start/oop-coffee-machine-start/main.py
@@ -1,3 +1,6 @@
+from re import T
+import re
+from turtle import width
 from unittest.main import main
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
@@ -5,8 +8,25 @@ from money_machine import MoneyMachine
 
 
 if __name__ == "__main__":
-    all_report = CoffeeMaker()
-    all_report.report()
-
+    coffee_maker = CoffeeMaker()
     money_machine = MoneyMachine()
-    money_machine.report()
+
+    menu = Menu()
+    is_on = True
+
+    while is_on:
+        option = menu.get_items()
+        choice = input(f'What would you like? ({option}): ')
+
+        if choice == "off":
+            is_on = False
+        elif choice == "report":
+            coffee_maker.report()
+            money_machine.report()
+        else:
+            drink = menu.find_drink(choice)
+            if coffee_maker.is_resource_sufficient(drink):
+                if money_machine.make_payment(drink.cost):
+                    coffee_maker.make_coffee(drink)
+            else:
+                print('no')
