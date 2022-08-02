aca3ecea32287c2d6d380a85965ea4a21049268a
Intira-72 <intira.vinit@gmail.com>
2022-08-02 14:00:13 +0700

    Day 23 Turtle Crossing - Create the Car behaviour

--- day-23/Turtle-Crossing/car_manager.py
+++ day-23/Turtle-Crossing/car_manager.py
@@ -1,7 +1,28 @@
+from turtle import Turtle
+import random
+
 COLORS = ["red", "orange", "yellow", "pink", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
 class CarManager:
-    pass
+    def __init__(self):
+        self.all_cars = []
+
+
+    def create_car(self):
+        random_chance = random.randint(1, 6)
+        if random_chance == 1:
+            new_car = Turtle("square")
+            new_car.shapesize(stretch_wid=1, stretch_len=2)
+            new_car.color(random.choice(COLORS))
+            new_car.penup()
+            new_car.goto(270, random.randint(-220, 230))
+
+            self.all_cars.append(new_car)
+
+
+    def car_move(self):
+        for car in self.all_cars:
+            car.backward(MOVE_INCREMENT)
--- day-23/Turtle-Crossing/main.py
+++ day-23/Turtle-Crossing/main.py
@@ -1,6 +1,7 @@
 import time
 from turtle import Screen
 from player import Player
+from car_manager import CarManager
 
 
 screen = Screen()
@@ -10,6 +11,7 @@ screen.title("Turtle Crossing")
 screen.tracer(0)
 
 player = Player()
+cars = CarManager()
 
 screen.listen()
 screen.onkey(player.go_up, "Up")
@@ -19,5 +21,7 @@ while game_is_on:
     time.sleep(0.1)
     screen.update()
 
+    cars.create_car()
+    cars.car_move()
 
 screen.exitonclick()
