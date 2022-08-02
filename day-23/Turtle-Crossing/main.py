63d7751cf57538828fabebc2bfb9fc35d46cf721
Intira-72 <intira.vinit@gmail.com>
2022-08-02 12:04:42 +0700

    Day 23 Turtle Crossing - Starting Code

---
+++ day-23/Turtle-Crossing/main.py
@@ -0,0 +1,18 @@
+import time
+from turtle import Screen
+
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("#15202b")
+screen.title("Turtle Crossing")
+screen.tracer(0)
+
+game_is_on = True
+
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+
+screen.exitonclick()
