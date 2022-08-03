a2db20c2f5f1016b115000850f9f8e943a72eb12
Intira-72 <intira.vinit@gmail.com>
2022-08-03 12:30:34 +0700

    basic file open, read, write, create new file

---
+++ day-24/main.py
@@ -0,0 +1,14 @@
+# with open("my_file.txt") as file:   #Read File for Readonly Mode
+#     print(file.read())
+
+    
+# with open("my_file.txt", "w") as file:
+#     file.write("New Text...")
+
+
+# with open("my_file.txt", "a") as file:
+#     file.write("\nNew Text Line...")
+
+
+with open("new_file.txt", "w") as file:
+    file.write("Create New File...")
