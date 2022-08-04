6d2b483acc3bfe07c18fdf05f14b02f355cb11ba
Intira-72 <intira.vinit@gmail.com>
2022-08-04 14:20:51 +0700

    The Great Squirrel Census Data Analysis (with Pandas)

--- day-25/main.py
+++ day-25/main.py
@@ -17,6 +17,7 @@
 #     print(temperatures)
 
 
+from cmath import nan
 import pandas
 
 # data = pandas.read_csv("weather_data.csv")
@@ -32,11 +33,24 @@ import pandas
 # print(monday.temp * 9/5 + 32)
 
 
-data_dict = {
-    'students': ['Amy', 'James', 'Angela'],
-    'scores': [76, 56, 65]
+# data_dict = {
+#     'students': ['Amy', 'James', 'Angela'],
+#     'scores': [76, 56, 65]
+# }
+
+# data = pandas.DataFrame(data_dict)
+# data.to_csv("new_data.csv")
+
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census.csv")
+color_data = data['Primary Fur Color'].to_list()
+
+color_list = list(set(color_data))[1:]
+count_list = [color_data.count(c) for c in color_list]
+
+new_value = {
+    'Fur Color': color_list,
+    'Count': count_list
 }
 
-data = pandas.DataFrame(data_dict)
-data.to_csv("new_data.csv")
-
+new_data_csv = pandas.DataFrame(new_value)
+new_data_csv.to_csv("squirrel_count.csv")
