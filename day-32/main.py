1c6d9f72b8d743d9657ded16b66cf4575680df4a
Intira-72 <intira.vinit@gmail.com>
2022-09-07 17:24:25 +0700

    day-32 basic smtplib send email day_of_motivational

---
+++ day-32/main.py
@@ -0,0 +1,29 @@
+import smtplib
+import random
+import datetime as dt
+
+EMAIL_HOST_USER = "enicma.shop2022@gmail.com"
+EMAIL_HOST_PASSWORD = "vnvfhimxqntdmpwc"
+
+def send_mail(quote, day_name):
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as connection:
+        connection.login(user=EMAIL_HOST_USER,
+                        password=EMAIL_HOST_PASSWORD)
+
+        connection.sendmail(from_addr=EMAIL_HOST_USER,
+                            to_addrs="hijika111@gmail.com",
+                            msg=f"Subject:{day_name} Motivation.\n\n{quote}")
+
+
+def day_of_motivation():
+    now = dt.datetime.now()
+
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
+
+    send_mail(quote, now.strftime("%A"))
+
+
+if __name__ == "__main__":
+    day_of_motivation()
